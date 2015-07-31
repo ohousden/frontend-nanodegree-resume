33b40d6271912675d7be5c8ea2f89ad42205f389
Owen Housden <owen.housden@capitalone.com>
2015-07-31 16:32:29 -0400

    refactored html and resumeBuilder to make it easier to read

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,119 @@
+function displayContacts() {
+    $('#contacts').append(HTMLmobile.replace("%data%", bio.contacts.mobileNumber));
+    $('#contacts').append(HTMLemail.replace("%data%", bio.contacts.email));
+    $('#contacts').append(HTMLgithub.replace("%data%", bio.contacts.githubUsername));
+    $('#contacts').append(HTMLlocation.replace("%data%", bio.contacts.location));
+    $('#footerContacts').append(HTMLmobile.replace("%data%", bio.contacts.mobileNumber));
+    $('#footerContacts').append(HTMLemail.replace("%data%", bio.contacts.email));
+    $('#footerContacts').append(HTMLgithub.replace("%data%", bio.contacts.githubUsername));
+    $('#footerContacts').append(HTMLlocation.replace("%data%", bio.contacts.location));
+    $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+}
+function displayMainInfo() {
+    $('#header').append(HTMLheaderName.replace("%data%", bio.name));
+    $('#header').append(HTMLheaderRole.replace("%data%", bio.role));
+    $("#header").append(HTMLcontactStart);
+    $('#header').append(HTMLbioPic.replace("%data%", bio.image));
+}
+
+function displayUtilities() {
+    $("#mapDiv").append(googleMap);
+}
+
+function displaySkills() {
+    $("#header").append(HTMLskillsStart);
+    for(skill in bio.skills){
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+        $("#skills").append(formattedSkill);
+    }
+}
+
+function inName(name) {
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+    return name[0] +" "+ name[1];
+}
+
+function displayWork() {
+    for(job in work.jobs){
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer+formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
+
+function displaySchools() {
+    $('#education').append(HTMLschoolStart);
+    for(school in education.schools) {
+        var schoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        $(".education-entry").append(schoolName);
+
+        var schoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        $(".education-entry").append(schoolDegree);
+
+        var schoolDates = HTMLschoolDates.replace("%data%", education.schools[school].datesAttended);
+        $(".education-entry").append(schoolDates);
+
+        var schoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        $(".education-entry").append(schoolLocation);
+
+        var schoolMajors = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        $(".education-entry").append(schoolMajors);
+
+        var schoolURL = HTMLonlineURL.replace("%data%","link").replace("#", education.schools[school].url);
+        $(".education-entry").append(schoolURL);
+    }
+}
+function displayProjects() {
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+
+function displayCourses(){
+    $('#education').append(HTMLonlineClasses);
+    $('#education').append(HTMLschoolStart);
+    for(course in education.onlineCourses) {
+        var courseTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+        $(".education-entry:last").append(courseTitle);
+
+        var courseSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+        $(".education-entry:last").append(courseSchool);
+
+        var courseDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+        $(".education-entry:last").append(courseDates);
+
+        var courseURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+        $(".education-entry:last").append(courseURL);
+    }
+}
+
 var bio = {
     "name": "Owen Housden",
     "role": "Front-End Developer",
