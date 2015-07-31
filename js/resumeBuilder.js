36c1a8b64e89c7885f4d3fe913d6ba407f61a7df
Owen Housden <owen.housden@capitalone.com>
2015-07-31 16:02:24 -0400

    initial commit of local work

---
+++ js/resumeBuilder.js
@@ -0,0 +1,95 @@
+var bio = {
+    "name": "Owen Housden",
+    "role": "Front-End Developer",
+    "image": "https://placeimg.com/300/300/people",
+    "welcomeMessage": "Hello, here is my welcome message",
+    "contacts": {
+        "mobileNumber": "804-694-9856",
+        "email": "ohousden@gmail.com",
+        "githubUsername": "ohousden",
+        "location": "Richmond, Va"
+    },
+    "skills": [
+        "Angular JS",
+        "Ruby",
+        "Javascript",
+        "HTML", "CSS",
+        "Jquery"
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Capital One",
+            "title": "QA Analyst",
+            "location": "Goochland, Va",
+            "dates": "2012-2015",
+            "description": "Sed vel justo eu sem euismod vulputate. Suspendisse dapibus vitae nibh nec lobortis. Quisque mattis lacus turpis, vel sagittis tortor tempus sit amet. Aliquam erat."
+        },
+        {
+            "employer": "G4S",
+            "title": "Security Officer",
+            "location": "Henrico, Va",
+            "dates": "2005-2012",
+            "description": "In congue orci turpis, vitae viverra magna volutpat eget. Duis molestie eros quis sem posuere, at hendrerit ligula rutrum. Maecenas in condimentum libero. Quisque facilisis."
+        }
+
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "Project Title 1",
+            "dates": "01/15/2014 - 01/25/2014",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget sodales arcu, at porta lectus. Vestibulum in purus tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras eleifend ex at arcu maximus gravida. Sed pellentesque auctor ligula, vitae volutpat lectus rhoncus quis. Suspendisse vehicula nibh lectus, at aliquet risus ultrices at. Mauris at condimentum magna. Nulla sed aliquet nunc, at aliquet enim. Vivamus non justo ut sem varius scelerisque. Donec dictum ligula at ante tincidunt, ac commodo est fringilla. Nulla vel nisi a erat porta fringilla. Nullam nec leo sed leo tristique ornare ac ac tortor. Mauris dignissim nunc in libero accumsan, et dictum neque rutrum. Phasellus et odio malesuada, tempor neque eu, porta orci. Etiam ullamcorper viverra nibh, et efficitur metus efficitur eu. Quisque risus eros, tincidunt vel libero sed, congue hendrerit lacus.",
+            "images": [
+                "https://placeimg.com/300/250/animals",
+                "https://placeimg.com/300/250/arch"
+            ]
+        },
+        {
+            "title": "Project Title 2",
+            "dates": "01/15/2014 - 01/25/2014",
+            "description": "Duis nec imperdiet nulla, ac tristique quam. Sed porta magna ligula. Aenean at ipsum eget ligula bibendum facilisis. Aliquam sed mauris nec ligula fermentum condimentum. Phasellus nunc velit, iaculis sed est vitae, vestibulum hendrerit sapien. Sed enim enim, tincidunt a accumsan sit amet, pellentesque nec neque. Etiam sed felis id dui fringilla mattis non vitae enim. Suspendisse accumsan urna nec odio ornare, in tincidunt purus vestibulum. Vestibulum ullamcorper, justo a convallis suscipit, justo magna condimentum arcu, fermentum fermentum enim arcu sit amet nisl. Morbi non rutrum ante, vel commodo nibh.",
+            "images": [
+                "https://placeimg.com/300/250/nature",
+                "https://placeimg.com/300/250/tech"
+            ]
+        }
+    ]
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "Manchester High School",
+            "location": "Chesterfield, Va",
+            "degree": "Diploma",
+            "majors": ["n/a"],
+            "datesAttended": "2003-2007",
+            "url": "http://www.manchesterhs.mychesterfieldschools.com/"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "online course 1 title",
+            "school": "school for online course 1",
+            "dates": "dates for course 1",
+            "url": "www.google.com"
+        },
+        {
+            "title": "online course 2 title",
+            "school": "school for online course 2",
+            "dates": "dates for course 2",
+            "url": "www.google.com"
+        },
+        {
+            "title": "online course 3 title",
+            "school": "school for online course 3",
+            "dates": "dates for course 3",
+            "url": "www.google.com"
+        }
+    ]
+}
